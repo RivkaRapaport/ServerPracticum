a2354ba8a187b32db41ca4d7e4b070234048bf5f
RivkaRapaport <145693086+RivkaRapaport@users.noreply.github.com>
2023-09-21 21:10:27 +0300

    Update Program.cs

--- MyProject.WebAPI/Program.cs
+++ MyProject.WebAPI/Program.cs
@@ -6,27 +6,18 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using MyProject.MyContext;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
-
-
 builder.Services.AddDbContext<IContex, MyDBContext>();
 builder.Services.AddServices();
-  
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddAuthorization();
-
 builder.Services.AddCors(option => option.AddPolicy("corsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 app.UseCors("corsPolicy");
 
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -34,9 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
